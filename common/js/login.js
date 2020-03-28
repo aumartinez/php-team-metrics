bf8e8b8e089aeb36914cf3e72aa9c11419d459fd
Augusto Martinez <acm_asd@hotmail.com>
2020-03-28 11:56:01 -0600

    updates

--- common/js/login.js
+++ common/js/login.js
@@ -16,6 +16,34 @@ $(document).ready(function(){
       evt.preventDefault();
       window.scrollTo(0, 0);
       $(".loader").removeClass("active");
+      return false;
     }
   });
+  
+  function validateForm() {
+    var errors = [];
+    
+    //Required fields    
+    if ($("#user").val().length == 0) {
+      errors.push["user"];
+    }
+    
+    if ($("#password").val().length == 0) {
+      errors.push["password"];
+    }
+    
+    return errors;
+  }
+  
+  function displayErrors(errors) {
+    for (var i = 0; i < errors.length; i++) {
+      $("#" + errors[i] + "\"").next().addClass("active");
+    }
+    
+    $("#error-div").text("Errors found!");
+  }
+  
+  function removeErrors() {
+    $(".error-mess.active").removeClass("active");
+  }
 });
