574148aec9a90b31dcfde1eba28db0699d9be2cb
Augusto Martinez <acm_asd@hotmail.com>
2020-03-28 21:07:44 -0600

    Update authmodel.php

--- app/models/authmodel.php
+++ app/models/authmodel.php
@@ -1,6 +1,7 @@
 <?php
 
-class Authmodel extends Dbmodel {  
+class Authmodel extends Dbmodel {
+  protected $sanitized = array();  
   
   public function login_required() {
     $required = LOGIN_REQUIRED;
@@ -15,6 +16,18 @@ class Authmodel extends Dbmodel {
     $this->error_check();
   }
   
+  # Global sanitize method
+  public function sanitize_post() {
+    $this->sanitized = array();
+    
+    foreach ($_POST as $key => $value) {
+      $this->sanitized[$key] = $this->open_link()->real_escape_string($value);
+    }
+    
+    return $this->sanitized;
+  }
+  
+  # Error check method
   protected function error_check() {
     if (count($_SESSION["error"]) > 0) {
       error_log("Error validating form", 0);
