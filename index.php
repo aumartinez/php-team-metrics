00eadd7a2dd31b708cec0bca3c9ee9ffc90fd6cc
Augusto Martinez <acm_asd@hotmail.com>
2020-03-30 09:09:06 -0600

    Update index.php

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <?php
 
 # Define current directory
-define("DS", DIRECTORY_SEPARATOR);
-define("ROOT", dirname(__FILE__) . DS . "app");
+define ("DS", DIRECTORY_SEPARATOR);
+define ("ROOT", dirname(__FILE__) . DS . "app");
 
 # Sanitize URL
 $url = filter_var($_SERVER["REQUEST_URI"], FILTER_SANITIZE_URL);
