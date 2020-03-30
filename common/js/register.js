ad3eb5db9c547d8ed18de248daf007551299a020
Augusto Martinez <acm_asd@hotmail.com>
2020-03-29 20:38:12 -0600

    updates

---
+++ common/js/register.js
@@ -0,0 +1,65 @@
+// Login validate
+
+$(document).ready(function(){
+  //To do
+    
+  $("#login-form").submit(function(evt){    
+    $(".loader").addClass("active");
+    var errors = validateForm();
+    
+    if (errors.length == 0) {
+      return true;
+    }
+    else {
+      removeErrors();
+      displayErrors(errors);
+      evt.preventDefault();
+      window.scrollTo(0, 0);
+      $(".loader").removeClass("active");      
+    }
+  });
+  
+  var clicked = false;
+  
+  $("#view-password").click(function(){   
+    if (clicked == false){
+      $("#password").attr("type", "text");
+      $("#view-password i").removeClass("fa-eye");
+      $("#view-password i").addClass("fa-eye-slash");
+      clicked = true;
+    }
+    else {
+      $("#password").attr("type", "password");
+      $("#view-password i").removeClass("fa-eye-slash");
+      $("#view-password i").addClass("fa-eye");
+      clicked = false;
+    }
+  });
+  
+  function validateForm() {    
+    var arr = [];
+        
+    //Required fields    
+    if ($("#user").val().length == 0) {
+      arr.push("user");
+    }
+    
+    if ($("#password").val().length == 0) {
+      arr.push("password");
+    }
+    
+    return arr;
+  }
+  
+  function displayErrors(errors) {
+    for (var i = 0; i < errors.length; i++) {
+      $("#" + errors[i]).next().addClass("active");
+    }
+    
+    $("#error-div").text("Errors found!");
+  }
+  
+  function removeErrors() {
+    $(".error-mess.active").removeClass("active");
+  }
+});
