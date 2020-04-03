f1c9ac768555d990663ac49455d859573095227b
Augusto Martinez <acm_asd@hotmail.com>
2020-04-03 15:59:46 -0600

    Update dbmodel.php

--- app/models/dbmodel.php
+++ app/models/dbmodel.php
@@ -24,7 +24,9 @@ class Dbmodel {
   
   # Create DB
   protected function create_db($dbname) {    
-    $sql = "CREATE DATABASE '{$dbname}' CHARSET = utf8 COLLATE = utf8_unicode_ci";
+    $sql = "CREATE DATABASE '{$dbname}'
+            CHARACTER SET utf8 
+            COLLATE utf8_unicode_ci";
     $this->set_query($sql);    
   }
   
