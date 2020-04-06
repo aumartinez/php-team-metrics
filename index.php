094622f60096fb81bd3b3689d7e14655916c20ac
Augusto Martinez <acm_asd@hotmail.com>
2020-04-06 09:34:02 -0600

    Update index.php

--- index.php
+++ index.php
@@ -8,6 +8,6 @@ define ("ROOT", dirname(__FILE__) . DS . "app");
 $url = filter_var($_SERVER["REQUEST_URI"], FILTER_SANITIZE_URL);
 
 # Load core
-require_once(ROOT . DS . "core" . DS . "core.php");
+require_once (ROOT . DS . "core" . DS . "core.php");
 
 ?>
