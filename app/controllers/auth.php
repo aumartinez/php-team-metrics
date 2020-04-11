35a5812e3096b5004ff06befe27600438f887260
Augusto Martinez <acm_asd@hotmail.com>
2020-04-10 18:24:32 -0600

    Update auth.php

--- app/controllers/auth.php
+++ app/controllers/auth.php
@@ -14,11 +14,10 @@ class Auth extends Controller {
     $_SESSION["error"] = array();
     
     # Any models required to interact with this controller should be loaded here    
-    $this->load_model("Authmodel");    
+    $this->load_model("Authmodel");  
   }
   
-  public function login() {
-    
+  public function login() {    
     # Check if referred URI is coming from login form
     if (!isset($_POST["submit-form"])) {
       $this->not_found();
@@ -30,9 +29,35 @@ class Auth extends Controller {
     # Initialize methods
     $this->get_model("Authmodel")->login_required();
     $this->get_model("Authmodel")->sanitize_post();
-    $this->get_model("Authmodel")->auth_user($this->get_model("Authmodel")->sanitized["user"], $this->get_model("Authmodel")->sanitized["password"]);
+    if ($this->get_model("Authmodel")->auth_user()) {
+      $this->get_model("Authmodel")->redirect(PATH . "/cpanel");
+    }
+    else {
+      $this->get_model("Authmodel")->redirect(PATH . "/login");
+    }
   }
- 
+  
+  public function startup() {
+    
+    if (!isset($_POST["submit-form"])) {
+      $this->not_found();
+    }
+    
+    $_SESSION["submit-form"] = true;
+    $_SESSION["error"] = array();
+    
+    # Initialize methods
+    $this->get_model("Authmodel")->sysadmin_required();
+    $this->get_model("Authmodel")->sanitize_post();
+    $this->get_model("Authmodel")->sysadmin_validate();
+    if ($this->get_model("Authmodel")->sysadmin_register()) {
+      $this->get_model("Authmodel")->redirect(PATH . "/success");
+    }
+    else {
+      $this->get_model("Authmodel")->redirect(PATH . "/dberror");
+    }
+    
+  } 
    
   public function not_found() {
     $this->get_model("Authmodel")->redirect(PATH . "/login");
