2ff582d95d5f4af2e0e79f16d9374bafb95fd470
Augusto Martinez <acm_asd@hotmail.com>
2020-04-12 15:28:03 -0600

    Update register.php

--- app/controllers/register.php
+++ app/controllers/register.php
@@ -10,8 +10,9 @@ class Register extends Controller {
     session_start();
             
     # Any models required to interact with this controller should be loaded here    
-    $this->load_model("Dbmodel");
-    $this->load_model("Pagemodel");
+    $this->load_model("DbModel");
+    $this->load_model("RegisterModel");
+    $this->load_model("PageModel");
     
     # Instantiate custom view output
     $this->output = new Pageview();
@@ -19,6 +20,8 @@ class Register extends Controller {
   
   # Each method will request the model to present the local resource
   public function index() {
+  
+  
     
     if (isset($_SESSION["logged"])) {      
       header("Location:/". PATH ."/cpanel");      
@@ -79,7 +82,7 @@ class Register extends Controller {
   
   # Controller/Model/View link
   protected function build_page($page_name) {    
-    $html_src = $this->get_model("Pagemodel")->get_page($page_name);    
+    $html_src = $this->get_model("PageModel")->get_page($page_name);    
     $html = $this->output->replace_localizations($html_src);
     
     $this->get_view()->render($html);
