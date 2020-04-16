fadee2c5d1f4edc623b2b190198b2a2238eb9d9f
Augusto Martinez <acm_asd@hotmail.com>
2020-04-16 10:46:30 -0600

    Create reset.js

---
+++ common/js/reset.js
@@ -0,0 +1,115 @@
+// Reset validate
+
+$(document).ready(function(){
+  //To do
+    
+  $("#reset-form").submit(function(evt){    
+    $(".loader").addClass("active");
+    var errors = validateForm();
+    
+    if (errors.length == 0) {
+      return true;
+    }
+    else {
+      removeErrors();
+      displayErrors(errors);
+      evt.preventDefault();
+      window.scrollTo(0, 0);
+      $(".loader").removeClass("active");      
+    }
+  });
+  
+  //Reveal password widget
+  var passclicked = false;
+  
+  $("#view-password").click(function(){   
+    if (passclicked == false){
+      $("#password").attr("type", "text");
+      $("#view-password i").removeClass("fa-eye");
+      $("#view-password i").addClass("fa-eye-slash");
+      passclicked = true;
+    }
+    else {
+      $("#password").attr("type", "password");
+      $("#view-password i").removeClass("fa-eye-slash");
+      $("#view-password i").addClass("fa-eye");
+      passclicked = false;
+    }
+  });
+  
+  var verifyclicked = false;
+  
+  $("#view-match").click(function(){   
+    if (verifyclicked == false){
+      $("#verify").attr("type", "text");
+      $("#view-match i").removeClass("fa-eye");
+      $("#view-match i").addClass("fa-eye-slash");
+      verifyclicked = true;
+    }
+    else {
+      $("#verify").attr("type", "password");
+      $("#view-match i").removeClass("fa-eye-slash");
+      $("#view-match i").addClass("fa-eye");
+      verifyclicked = false;
+    }
+  });
+  
+  function validateForm() {    
+    var err = [];
+        
+    //Required fields    
+    if ($("#email").val().length == 0) {
+      err.push("user");
+    }
+    
+    if ($("#password").val().length == 0) {
+      err.push("password");
+    }
+    
+    if ($("#verify").val().length == 0) {
+      err.push("password");
+    }
+    
+    //Validate email
+    var email = $("#email").val();
+    var regExp = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    var testEmail = regExp.exec(email);
+
+    if (!testEmail || testEmail == null) {
+       err.push("email");
+    }
+    
+    //Validate password length
+    if($("#password").val().length < 6) {
+      err.push("password");
+    }
+    
+    //Validate password complexity (1 letter, 1 number)
+    var password = $("#password").val();
+    var input = /^(?=.*[0-9])(?=.*[a-zA-Z])([a-zA-Z0-9]+)$/;
+    var testPass = input.exec(password);
+    
+    if (!testPass || testPass == null) {
+      err.push("password");
+    }
+
+    //Validate password match
+    if($("#password").val() !== $("#verify").val()) {
+      err.push("verify");
+    }
+    
+    return err;
+  }
+  
+  function displayErrors(errors) {
+    for (var i = 0; i < errors.length; i++) {
+      $("#" + errors[i]).next().addClass("active");
+    }
+    
+    $("#error-div").text("Errors found!");
+  }
+  
+  function removeErrors() {
+    $(".error-mess.active").removeClass("active");
+  }
+});
