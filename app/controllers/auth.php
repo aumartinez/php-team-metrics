65c996a17436e7e6b98fb4db53d895c751a1b51d
Augusto Martinez <acm_asd@hotmail.com>
2020-04-15 18:39:29 -0600

    Update auth.php

--- app/controllers/auth.php
+++ app/controllers/auth.php
@@ -15,30 +15,12 @@ class Auth extends Controller {
     
     # Any models required to interact with this controller should be loaded here    
     $this->load_model("AuthModel");  
+    $this->load_model("EmailModel");
+    
   }
   
   public function index() {
     $this->get_model("AuthModel")->redirect(PATH . "/login");
-  }
-  
-  public function login() {    
-    # Check if referred URI is coming from login form
-    if (!isset($_POST["submit-form"])) {
-      $this->not_found();
-    }
-    
-    $_SESSION["submit-form"] = true;    
-    $_SESSION["error"] = array();
-    
-    # Initialize methods
-    $this->get_model("AuthModel")->login_required();
-    $this->get_model("AuthModel")->sanitize_post();
-    if ($this->get_model("AuthModel")->auth_user()) {
-      $this->get_model("AuthModel")->redirect(PATH . "/cpanel");
-    }
-    else {
-      $this->get_model("AuthModel")->redirect(PATH . "/login");
-    }
   }
   
   public function startup() {
@@ -64,6 +46,68 @@ class Auth extends Controller {
     }
     
   } 
+  
+  public function login() {    
+    # Check if referred URI is coming from login form
+    if (!isset($_POST["submit-form"])) {
+      $this->not_found();
+    }
+    
+    $_SESSION["submit-form"] = true;    
+    $_SESSION["error"] = array();
+    
+    # Initialize methods
+    $this->get_model("AuthModel")->login_required();
+    $this->get_model("AuthModel")->sanitize_post();
+    if ($this->get_model("AuthModel")->auth_user()) {
+      $this->get_model("AuthModel")->redirect(PATH . "/cpanel");
+    }
+    else {
+      $this->get_model("AuthModel")->redirect(PATH . "/login");
+    }
+  }
+  
+  public function register() {
+    if (!isset($_POST["submit-form"])) {
+      $this->not_found();
+    }
+    
+    $_SESSION["submit-form"] = true;
+    $_SESSION["error"] = array();
+    
+    $this->get_model("AuthModel")->register_required();
+    $this->get_model("AuthModel")->sanitize_post();
+    $this->get_model("AuthModel")->register_validate();
+    if ($this->get_model("AuthModel")->register_data()) {
+      $this->get_model("AuthModel")->redirect(PATH . "/success/pending");
+    }
+    else {
+      $this->get_model("AuthModel")->redirect(PATH . "/register");
+    }    
+  }
+  
+  public function email() {
+    if (!isset($_POST["submit-form"])) {
+      $this->not_found();
+    }
+    
+    $_SESSION["submit-form"] = true;
+    $_SESSION["error"] = array();
+    
+    $this->get_model("AuthModel")->recovery_required();
+    $this->get_model("AuthModel")->sanitize_post();
+    $this->get_model("AuthModel")->recovery_validate();
+    $hash = $this->get_model("AuthModel")->recovery_data();    
+    $email = $this->get_model("AuthModel")->sanitized["email"];    
+    
+    if ($this->get_model("EmailModel")->submit_recover($email, $hash)) {
+      $this->get_model("AuthModel")->redirect(PATH . "/success/recover");
+    }
+    else {
+      $this->get_model("AuthModel")->redirect(PATH . "/recover");
+    }
+    
+  }
    
   public function not_found() {
     $this->get_model("AuthModel")->redirect(PATH . "/login");
